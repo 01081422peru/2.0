675703b74f92dc64cdb43a0e142e3a8baef77cd6
01081422peru <ulianaandantoha@gmail.com>
2021-09-15 14:40:47 +0300

    hello world

---
+++ Лабораторна 2.1/Source.cpp
@@ -0,0 +1,26 @@
+﻿// Lab_02.cpp
+// <Перун, Уляна >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 16.
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    double Pi = 4. * atan(1.), x, z1, z2;
+
+	cout << "x = ";
+	cin >> x;
+
+	z1 = ((x * x + 2. * x) - 3. + (x + 1.) * sqrt(x * x - 9.)) / ((x * x - 2. * x) - 3. + (x - 1.) * sqrt(x * x - 9.));
+
+	z2 = sqrt(x + 3.) / sqrt(x - 3.); 
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
